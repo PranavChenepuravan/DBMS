c4782ec161bc6f2e130f3513b5dabca895a6aca7
PranavChenepuravan <117366861+PranavChenepuravan@users.noreply.github.com>
2023-03-14 14:42:24 +0530

    Add files via upload

---
+++ text.sql
@@ -0,0 +1,10 @@
+create database sample;
+use sample;
+create table sample1 (id integer,name varchar(20));
+insert into sample1 values(1,"pranav"); 
+insert into sample1 values(2,"hari");
+insert into sample1 values(3,"faheem");
+
+drop database students;
+use student
+select * from sample1;
