cbc20dcb5fab85fac4860c484fab705a641b8dae
PranavChenepuravan <117366861+PranavChenepuravan@users.noreply.github.com>
2023-03-31 14:22:53 +0530

    Add files via upload

---
+++ Strength.sql
@@ -0,0 +1,19 @@
+create database Subjects;
+use Subjects;
+create table section(s_id int,s_name varchar(20),strength int);
+insert into section values(1,'Computer Science',20);
+insert into section values(2,'Portal',45);
+insert into section values(3,'Machine Learning',60);
+DELIMITER //
+CREATE FUNCTION totalStrength()
+RETURNS INT 
+DETERMINISTIC
+BEGIN
+  DECLARE total INT;
+  SET total = 0;
+  SELECT SUM(strength) INTO total FROM section;
+  RETURN total;
+END;
+//
+DELIMITER ;
+SELECT totalStrength();
